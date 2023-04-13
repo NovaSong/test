539d0c7b14c11824e7135422051e899cb58f1cd8
snova <ricardo5179@outlook.com>
2023-04-13 10:40:18 +0800

    test

--- main.go
+++ main.go
@@ -4,53 +4,43 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"time"
 )
 
 func main() {
-	for {
-		fuckthenet()
-		time.Sleep(3 * time.Second)
-	}
-}
-
-func fuckthenet() {
 	url := "http://www.google.com"
-
-	for i := 0; i < 100000; i++ {
-
-		go DoARequest(url)
-		// time.Sleep(1 * time.Second)
-	}
-}
-
-func DoARequest(url string) {
-
 	client := &http.Client{}
 	reqest, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Println(err)
 	}
-
-	//处理返回结果
-	response, err := client.Do(reqest)
-	if err != nil {
-		log.Println(err)
-	}
-	if response != nil {
-		status := response.StatusCode
-		if status == 200 {
-			f, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-
-			if err != nil {
-				log.Println(err)
-			}
-			defer f.Close()
-			//设置日志输出到 f
-			log.SetOutput(f)
-			log.Println(status)
+	for {
+		for i := 0; i < 30000; i++ {
+			go func() {
+				//处理返回结果
+				response, err := client.Do(reqest)
+				if err != nil {
+					log.Println(err)
+				}
+				if response != nil {
+					status := response.StatusCode
+					if status == 200 {
+						f, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+						if err != nil {
+							log.Println(err)
+						}
+						defer f.Close()
+						//设置日志输出到 f
+						log.SetOutput(f)
+						log.Println(status)
+					}
+					log.Println(status)
+				}
+			}()
+			// time.Sleep(1 * time.Second)
 		}
-		log.Println(status)
+		time.Sleep(65 * time.Second)
+		runtime.GC()
 	}
-
 }
